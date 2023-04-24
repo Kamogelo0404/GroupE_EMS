5f32db8f36c5c362ccaa51ebd8e40016547dde67
monaSaurusRex <monqiquenagel24.mn@gmail.com>
2023-04-24 09:23:16 +0200

    Created project structure and components and added routing for employee management pages.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { ViewAllEmpComponent } from './components/view-all-emp/view-all-emp.component';
+import { ViewSingleEmpComponent } from './components/view-single-emp/view-single-emp.component';
+import { CreateEmpComponent } from './components/create-emp/create-emp.component';
+import { EditEmpComponent } from './components/edit-emp/edit-emp.component';
+import { DeleteEmpComponent } from './components/delete-emp/delete-emp.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component:HomeComponent},
+  { path:'view-all-emp', component:ViewAllEmpComponent},
+  { path:'view-emp', component:ViewSingleEmpComponent},
+  {path:'create-emp', component:CreateEmpComponent},
+  {path:'edit-emp', component:EditEmpComponent},
+  {path:'delete-emp', component:DeleteEmpComponent},
+  {path: '**', redirectTo: '', pathMatch: 'full'} //default page used if page doesn't exist
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { ViewAllEmpComponent } from './components/view-all-emp/view-all-emp.component';
+import { ViewSingleEmpComponent } from './components/view-single-emp/view-single-emp.component';
+import { EditEmpComponent } from './components/edit-emp/edit-emp.component';
+import { DeleteEmpComponent } from './components/delete-emp/delete-emp.component';
+import { CreateEmpComponent } from './components/create-emp/create-emp.component';
+import { HeaderComponent } from './components/header/header.component';
+import { EmployeeFormComponent } from './components/employee-form/employee-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ViewAllEmpComponent,
+    ViewSingleEmpComponent,
+    EditEmpComponent,
+    DeleteEmpComponent,
+    CreateEmpComponent,
+    HeaderComponent,
+    EmployeeFormComponent
   ],
   imports: [
     BrowserModule,
