5bdf5bee2ec2babb8c21a7d89bfc5b474b5985e1
Monique <moniquenagel24.mn@gmail.com>
2023-04-30 18:01:57 +0200

    Created submit button component.

--- src/app/components/submit-button/submit-button.component.ts
+++ src/app/components/submit-button/submit-button.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { NavigationService } from 'src/app/services/navigation.service';
 
 @Component({
   selector: 'app-submit-button',
@@ -7,4 +9,9 @@ import { Component } from '@angular/core';
 })
 export class SubmitButtonComponent {
 
+  constructor(private router:Router, private navigation:NavigationService ){}
+
+  submit(){
+    this.navigation.back();
+  }
 }
