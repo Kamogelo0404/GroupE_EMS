72f950f62a77f2cc2bafb79af3e5df41f54a58fe
Monique <moniquenagel24.mn@gmail.com>
2023-04-30 17:17:00 +0200

    Fixed employee form component to have a better layout.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { HomeComponent } from './pages/home/home.component';
 import { ViewAllEmpComponent } from './pages/view-all-emp/view-all-emp.component';
 import { ViewSingleEmpComponent } from './pages/view-single-emp/view-single-emp.component';
@@ -32,7 +31,6 @@ import { DeleteButtonComponent } from './components/delete-button/delete-button.
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
