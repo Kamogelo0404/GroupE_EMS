b1bd915b999281e23c09efed01637672977d88ae
Monique <monqiquenagel24.mn@gmail.com>
2023-05-02 14:27:36 +0200

    Upadated app.module folder for httpclient functions.

--- Frontend/src/app/pages/view-all-emp/view-all-emp.component.ts
+++ Frontend/src/app/pages/view-all-emp/view-all-emp.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Employee } from 'src/app/interface/employee';
+import { EmployeeService } from 'src/app/services/employee/employee.service';
 
 @Component({
   selector: 'app-view-all-emp',
   templateUrl: './view-all-emp.component.html',
-  styleUrls: ['./view-all-emp.component.css']
+  styleUrls: ['./view-all-emp.component.css'],
 })
-export class ViewAllEmpComponent {
+export class ViewAllEmpComponent{
 
+  // employees: Employee[];
+
+  // constructor(private employeeService: EmployeeService,private router: Router) {}
+
+  // ngOnInit(): void {
+  //   this.getAllEmployees();
+  // }
+
+  // private getAllEmployees(){
+  //   this.employeeService.getAllEmployees().subscribe(data => {
+  //     this.employees = data;
+  //   })
+  // }
 }
