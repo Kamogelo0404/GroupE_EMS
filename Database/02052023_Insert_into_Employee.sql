1d065d7fbbaae27133265222c7723f8320bf6f3f
Donnedoni <donnedonifillies@gmail.com>
2023-05-02 12:29:02 +0200

    fixed the database

--- Database/02052023_Insert_into_Employee.sql
+++ Database/02052023_Insert_into_Employee.sql
@@ -3,4 +3,4 @@ values('Pontsho', 'Moekwa', 'P@panrass.com', '0123456789'),
 	  ('Doni','Fillies','d@gmail.com','0123450089'),
 	  ('Augustine', 'Khumalo', 'augustine.khumalo@gmail.com', '0112345678'),
 	  ('Kamogelo', 'Nkadimeng', 'kamogelonkadimeng0404@gmail.com', '0212359878'),
-	  ('Richard', 'Johnson', 'r.Johnson@gmail.com', '0118999881') 
+	  ('Richard', 'Johnson', 'r.Johnson@gmail.com', '0118999881')
