455048b0fb851a824bb78f9a81f9ef7584467707
Monique <monqiquenagel24.mn@gmail.com>
2023-05-02 12:05:45 +0200

    Created front-end folder to organise front-end related files within it.

---
+++ Frontend/src/app/components/submit-button/submit-button.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { NavigationService } from 'src/app/services/navigation/navigation.service';
+
+@Component({
+  selector: 'app-submit-button',
+  templateUrl: './submit-button.component.html',
+  styleUrls: ['./submit-button.component.css']
+})
+export class SubmitButtonComponent {
+
+  constructor(private router:Router, private navigation:NavigationService ){}
+
+  submit(){
+    this.navigation.back();
+  }
+}
---
+++ Frontend/src/app/pages/delete-emp/delete-emp.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { NavigationService } from 'src/app/services/navigation/navigation.service';
+
+@Component({
+  selector: 'app-delete-emp',
+  templateUrl: './delete-emp.component.html',
+  styleUrls: ['./delete-emp.component.css']
+})
+export class DeleteEmpComponent {
+  constructor(private router:Router){}
+
+  confirmDelete(){
+    this.router.navigate(['/view-all-emp']);
+  }
+}
