a35b45e95091564fc3e161689fa951c2ba2bd20e
Donnedoni <donnedonifillies@gmail.com>
2023-05-02 09:50:49 +0200

    added the database script to the database folder

---
+++ Database/02052023_0826_Create_Table_Employee.sql
@@ -0,0 +1,10 @@
+create table if not exists employee(
+id int generated always as identity not null,
+firstName VARCHAR(25) not null,
+lastName VARCHAR (25)not null,
+email VARCHAR(255) not null,
+contactNo VARCHAR(10) not null,
+primary key(id)
+);
+
+
