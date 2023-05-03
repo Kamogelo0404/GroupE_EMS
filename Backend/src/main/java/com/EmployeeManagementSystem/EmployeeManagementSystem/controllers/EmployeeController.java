ad67abdc029fa1a755e603ed093e770794f51e84
KhumaloAugustine <mr.a.s.khumalo@gmail.com>
2023-05-03 12:02:34 +0200

    Added configuration file for CORS

--- Backend/src/main/java/com/EmployeeManagementSystem/EmployeeManagementSystem/controllers/EmployeeController.java
+++ Backend/src/main/java/com/EmployeeManagementSystem/EmployeeManagementSystem/controllers/EmployeeController.java
@@ -15,6 +15,7 @@ import com.EmployeeManagementSystem.EmployeeManagementSystem.exceptions.Resource
 import com.EmployeeManagementSystem.EmployeeManagementSystem.models.Employee;
 import com.EmployeeManagementSystem.EmployeeManagementSystem.repo.EmployeeRepo;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -23,6 +24,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 
+@CrossOrigin
 @RestController
 @RequestMapping("/employees")
 public class EmployeeController {
