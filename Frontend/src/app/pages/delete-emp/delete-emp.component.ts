4df0361f1b0b35507ba2bf847eb701d395680c88
Lesiba Moekwa <damacm1139@DAs-MacBook-Air.local>
2023-05-05 15:19:18 +0200

    soft delete

--- Frontend/src/app/pages/delete-emp/delete-emp.component.ts
+++ Frontend/src/app/pages/delete-emp/delete-emp.component.ts
@@ -11,6 +11,7 @@ import { NavigationService } from 'src/app/services/navigation/navigation.servic
 })
 export class DeleteEmpComponent implements OnInit {
  id:any;
+ 
  employee: Employee = new Employee();
   constructor(private employeeService: EmployeeService, 
     private router:Router,
@@ -29,6 +30,7 @@ export class DeleteEmpComponent implements OnInit {
   
     this.employeeService.getEmployeeById(this.id).subscribe(data => {
       console.log(data);
+      this.employee.deleted = true;
       this.employee = data;
     }, error => console.log(error));
 
@@ -39,6 +41,7 @@ export class DeleteEmpComponent implements OnInit {
     this.employeeService.editEmployee(id, this.employee).subscribe( data => {
       console.log(data);
      this.getEmployee(this.id = this.route.snapshot.params['id']); 
+     //this.employee.deleted = true;
      this.router.navigate(['./view-all-emp']);
     });
    
