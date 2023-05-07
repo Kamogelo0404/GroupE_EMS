960d5da843c07f6ef424f37e6e81bd2037d3f4e9
Monique <moniquenagel24.mn@gmail.com>
2023-05-07 15:30:20 +0200

    Fixed formatting of code layout.

--- Frontend/src/app/pages/delete-emp/delete-emp.component.ts
+++ Frontend/src/app/pages/delete-emp/delete-emp.component.ts
@@ -11,60 +11,55 @@ import Swal from 'sweetalert2';
   styleUrls: ['./delete-emp.component.css'],
 })
 export class DeleteEmpComponent implements OnInit {
- id:any;
- 
- employee: Employee = new Employee();
-  constructor(private employeeService: EmployeeService, 
-    private router:Router,
-    private route:ActivatedRoute,
-    private navigation:NavigationService){}
+  id: any;
 
-    back():void{
-      this.navigation.back();
-    }
+  employee: Employee = new Employee();
+  constructor(
+    private employeeService: EmployeeService,
+    private router: Router,
+    private route: ActivatedRoute,
+    private navigation: NavigationService
+  ) {}
+
+  back(): void {
+    this.navigation.back();
+  }
   ngOnInit(): void {
     this.getEmployee((this.id = this.route.snapshot.params['id']));
   }
 
-  private getEmployee(id:any){
-  
-    this.employeeService.getEmployeeById(this.id).subscribe(data => {
-      console.log(data);
-      this.employee = data;
-    }, error => console.log(error));
-
+  private getEmployee(id: any) {
+    this.employeeService.getEmployeeById(this.id).subscribe(
+      (data) => {
+        console.log(data);
+        this.employee = data;
+      },
+      (error) => console.log(error)
+    );
   }
 
-
-  confirmDelete(id:any){
-    this.employeeService.deleteEmployee(id).subscribe( data => {
+  confirmDelete(id: any) {
+    this.employeeService.deleteEmployee(id).subscribe((data) => {
       console.log(data);
       //this.employee.deleted = true;
-     this.getEmployee(this.id = this.route.snapshot.params['id']); 
-     
-     Swal.fire({
-      icon: 'warning',
-      title: 'deleted',
-      confirmButtonColor: '#d33',
-      showClass: {
-        popup: 'animate__animated animate__fadeInDown'
-      },
-      hideClass: {
-        popup: 'animate__animated animate__fadeOutUp'
-      },
-      customClass: {
-        confirmButton: 'btn btn-success'
-      },
-      confirmButtonText: '<i class="fa fa-trash"></i> OK'
+      this.getEmployee((this.id = this.route.snapshot.params['id']));
+
+      Swal.fire({
+        icon: 'warning',
+        title: 'Successfully Removed Employee',
+        confirmButtonColor: '#d33',
+        showClass: {
+          popup: 'animate__animated animate__fadeInDown',
+        },
+        hideClass: {
+          popup: 'animate__animated animate__fadeOutUp',
+        },
+        customClass: {
+          confirmButton: 'btn btn-success',
+        },
+        confirmButtonText: '<i class="fa fa-trash"></i> OK',
+      });
+      this.router.navigate(['./view-all-emp']);
     });
-    
-      
-      //confirmButtonColor: '#d33',
-      
-    
-     this.router.navigate(['./view-all-emp']);
-    });   
-  } 
-
-
+  }
 }
--- Frontend/src/app/pages/edit-emp/edit-emp.component.ts
+++ Frontend/src/app/pages/edit-emp/edit-emp.component.ts
@@ -7,75 +7,61 @@ import Swal from 'sweetalert2';
 @Component({
   selector: 'app-edit-emp',
   templateUrl: './edit-emp.component.html',
-  styleUrls: ['./edit-emp.component.css']
+  styleUrls: ['./edit-emp.component.css'],
 })
-export class EditEmpComponent implements OnInit{
-
+export class EditEmpComponent implements OnInit {
   constructor(
     private employeeService: EmployeeService,
     private router: Router,
-    private route: ActivatedRoute 
+    private route: ActivatedRoute
   ) {
-    this.route.paramMap.subscribe(paraMap => {
-      console.log(paraMap.get('id'))
-    
-    })
+    this.route.paramMap.subscribe((paraMap) => {
+      console.log(paraMap.get('id'));
+    });
   }
   id: any;
   employee: Employee = new Employee();
 
-
-
-  ngOnInit():void{
-  
-    this.getEmployee(this.id = this.route.snapshot.params['id']);
-   //this.getEmployee(this.id=44)
-    ;
-
+  ngOnInit(): void {
+    this.getEmployee((this.id = this.route.snapshot.params['id']));
+    //this.getEmployee(this.id=44)
   }
 
-  private getEmployee(id: any){
+  private getEmployee(id: any) {
     //this.id=44;
-    this.employeeService.getEmployeeById(this.id).subscribe(data => {
-     
-      this.employee = data;
-      console.log(data);
-    }, error => console.log(error));
-
+    this.employeeService.getEmployeeById(this.id).subscribe(
+      (data) => {
+        this.employee = data;
+        console.log(data);
+      },
+      (error) => console.log(error)
+    );
   }
 
-  update()
-  {
-    this.employeeService.editEmployee(this.id, this.employee).subscribe
-    (data => {
-      this.goToEmployeeList();
-      Swal.fire({
-        icon: 'success',
-        title: 'Updated',
-        showClass: {
-          popup: 'animate__animated animate__fadeInDown'
-        },
-        hideClass: {
-          popup: 'animate__animated animate__fadeOutUp'
-        },
-        customClass: {
-          confirmButton: 'btn btn-success'
-        },
-        confirmButtonText: '<i class="fas fa-check-circle"></i> OK'
-      });
-      
-    }, error => console.log(error))
-
+  update() {
+    this.employeeService.editEmployee(this.id, this.employee).subscribe(
+      (data) => {
+        this.goToEmployeeList();
+        Swal.fire({
+          icon: 'success',
+          title: 'Successfully Updated Employee',
+          showClass: {
+            popup: 'animate__animated animate__fadeInDown',
+          },
+          hideClass: {
+            popup: 'animate__animated animate__fadeOutUp',
+          },
+          customClass: {
+            confirmButton: 'btn btn-success',
+          },
+          confirmButtonText: '<i class="fas fa-check-circle"></i> OK',
+        });
+      },
+      (error) => console.log(error)
+    );
   }
 
-  goToEmployeeList()
-  {
-    this.router.navigate(['./view-all-emp'])
+  goToEmployeeList() {
+    this.router.navigate(['./view-all-emp']);
   }
-
-
-
-  
 }
-
-
